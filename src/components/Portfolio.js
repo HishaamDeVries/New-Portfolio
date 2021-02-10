7a3d555304c146e5b4c977ef7f96db15ddbb6574
Hishaam De Vries <hishaamdevries@gmail.com>
2021-02-10 10:54:10 +0200

    Checking something

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,23 +11,23 @@ export default class Porfolio extends Component {
               
             <div className="columns portfolio-item">
               <div className="item-wrap">
-                  <img src="images/githubFinder.png" className="item-img" alt="Github Finder"/>
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>GitHub Finder</h5>
-                      <p>Web App that is made to find github user profiles and some of the basic info therein.</p>
-                        <p>
-                      <a
-                        href="//https://github-finder-hishaam-version.netlify.app/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                      View Demo
-                      </a>
-                      </p>
-
-                    </div>
-                   </div>
+                <img src="images/githubFinder.png" className="item-img" alt="Github Finder"/>
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>GitHub Finder</h5>
+                    <p>Web App that is made to find github user profiles and some of the basic info therein.</p>
+                  </div>
+                </div>
+              </div>
+              <div className = "livedemo">
+                <section>
+                  <a href="//https://github-finder-hishaam-version.netlify.app/"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                    <button class="button button1">View Demo</button>
+                  </a>
+                </section>
               </div>
             </div>
 
@@ -38,105 +38,111 @@ export default class Porfolio extends Component {
                     <div className="portfolio-item-meta">
                       <h5>Contact Keeper</h5>
                       <p>Web app that creates and sorts user contacts.</p>
-                      <p style={{backgroundcolor: "white"}}> 
-                      <a
-                        href="https://protected-castle-62025.herokuapp.com/login"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                      View Demo
-                      </a>
-                      </p>
                     </div>
-                   </div>
+                  </div>
+                </div>
+              <div className = "livedemo">
+                <section>
+                  <a href="https://protected-castle-62025.herokuapp.com/login"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                    <button class="button button1">View Demo</button>
+                  </a>
+                </section>
               </div>
             </div>
 
             <div className="columns portfolio-item">
               <div className="item-wrap">
-                  <img src="images/it-logger.png" className="item-img" alt="IT Logger"/>
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>IT logger</h5>
-                      <p>Web App that is used to create logs for IT problems and Solutions in the workplace</p>
-                    </div>
-                   </div>
+                <img src="images/it-logger.png" className="item-img" alt="IT Logger"/>
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>IT logger</h5>
+                    <p>Web App that is used to create logs for IT problems and Solutions in the workplace</p>
+                  </div>
+                </div>
               </div>
-              <button> 
-                <a
-                  href="https://github.com/HishaamDeVries/it-logger"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Go To Repo
-                </a>
-              </button>
+              <div className = "livedemo">
+                <section>
+                  <a href="https://github.com/HishaamDeVries/it-logger"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                    <button class="button button1">Repo</button>
+                  </a>
+                </section>
+              </div>
             </div>
-            </div>
+            
+          </div>
 
-            <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf ">
+          <div id="portfolio-wrapper" className="bgrid-thirds s-bgrid-thirds cf ">
 
             <div className="columns portfolio-item">
               <div className="item-wrap">
-                  <img src="images/pointOfSale.png" className="item-img" alt="Point Of Sale"/>
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>POS System</h5>
-                      <p>Web App that is made for small or self-owned businesses to track transactions and inventory within the store.</p>
-                      <p> 
-                      <a
-                        href="https://hishaams-point-of-sale-system.herokuapp.com/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                      View Demo
-                      </a>
-                      </p>
-                    </div>
-                   </div>
+                <img src="images/pointOfSale.png" className="item-img" alt="Point Of Sale"/>
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>POS System</h5>
+                    <p>Web App that is made for small or self-owned businesses to track transactions and inventory within the store.</p>
+                  </div>
+                </div>
               </div>
-            </div>
-
-            <div className="columns portfolio-item">
-              <div className="item-wrap">
-                  <img src="images/Lottery.jpg" className="item-img" alt="Lottery"/>
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>Python Lottery App</h5>
-                      <p>Exercutable Application that takes 6 numbers and match
-                      with six random numbers to see if you win anything.</p>
-                      <p> 
-                      <a
-                        href="https://github.com/HishaamDeVries/python-lottery-exe/raw/master/Lottery.exe"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                      Download App
-                      </a>
-                      </p>
-                    </div>
-                   </div>
+              <div className = "livedemo">
+                <section>
+                  <a href="https://hishaams-point-of-sale-system.herokuapp.com/"
+                     target="_blank"
+                     rel="noopener noreferrer"
+                  >
+                    <button class="button button1">View Demo</button>
+                  </a>
+                </section>
               </div>
             </div>
 
             <div className="columns portfolio-item">
               <div className="item-wrap">
-                  <img src="images/Portfolio.png" className="item-img" alt="Portfolio"/>
-                  <div className="overlay">
-                    <div className="portfolio-item-meta">
-                      <h5>First Portfolio</h5>
-                      <p>First portfolio I made using React on Github Pages</p>
-                      <p> 
-                      <a
-                        href="https://hishaamdevries.github.io/portfolio/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                      View Demo
-                      </a>
-                      </p>
-                    </div>
-                   </div>
+                <img src="images/Lottery.jpg" className="item-img" alt="Lottery"/>
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>Python Lottery App</h5>
+                    <p>Exercutable Application that takes 6 numbers and match
+                    with six random numbers to see if you win anything.</p>
+                  </div>
+                </div>
+              </div>
+              <div className = "livedemo">
+                <section>
+                  <a  href="https://github.com/HishaamDeVries/python-lottery-exe/raw/master/Lottery.exe"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                    <button class="button button1">Download App</button>
+                  </a>
+                </section>
+              </div>
+            </div>
+
+            <div className="columns portfolio-item">
+              <div className="item-wrap">
+                <img src="images/Portfolio.png" className="item-img" alt="Portfolio"/>
+                <div className="overlay">
+                  <div className="portfolio-item-meta">
+                    <h5>First Portfolio</h5>
+                    <p>First portfolio I made using React on Github Pages</p>
+                  </div>
+                </div>
+              </div>
+              <div className = "livedemo">
+                <section>
+                  <a  href="https://hishaamdevries.github.io/portfolio/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                    <button class="button button1">View Demo</button>
+                  </a>
+                </section>
               </div>
             </div>
 
